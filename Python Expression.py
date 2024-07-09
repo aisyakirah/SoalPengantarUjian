d192a5b5fca69ecb58bf4d835d73783257ab921b
aisyakirah <168515561+aisyakirah@users.noreply.github.com>
2024-07-09 14:25:34 +0700

    Add files via upload

---
+++ Python Expression.py
@@ -0,0 +1,15 @@
+# a. 15 mod 5
+print("a.", 15 % 5)  # Hasil: 0
+
+# b. 12 +3*5==75
+print("b.", 12 + 3*5 == 75)  # Hasil: False
+
+# c. “PML”+”15523”
+print("c. PML" + "15523")  # Hasil: PML15523
+
+# d. “100”+234
+print("d. 100" + str(234))  # Hasil: 100234
+
+# e. ((11%3)+2) != 8/2
+hasil_e = (((11 % 3) + 2) != 8 / 2)
+print("e.", hasil_e)  # Hasil: True
